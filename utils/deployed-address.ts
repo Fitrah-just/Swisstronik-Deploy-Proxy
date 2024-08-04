df02936365102c6407c7bcf4990edb9e86a50f6c
FItrah Ramadhan <84637046+Fitrah-just@users.noreply.github.com>
2024-08-04 20:55:17 +0700

    Add files via upload

---
+++ utils/deployed-address.ts
@@ -0,0 +1,4 @@
+export const SWTRProxy = '0x9eA9f9E86313b8bddc7820CF79ad5763AB80155e'
+export const ProxyAdmin = '0xB9E9B91A29240e78504aE98fC1e200c64045323b'
+export const SWTRImplementation = '0x4262e6559C96b07dDAcC278D2dC0102ab2Ce56EF'
+    
